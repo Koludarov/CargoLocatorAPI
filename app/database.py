b458e8c3879671bc21005e0d52d3847891691d37
Artem <memskov@gmail.com>
2023-05-26 23:57:29 +0200

    .env file added

--- app/database.py
+++ app/database.py
@@ -1,16 +1,26 @@
 import logging
+import os
 
 import asyncpg
+from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
 
 
 async def startup(app):
     app.db_pool = await asyncpg.create_pool(
-        user="postgres",
-        password="postgres",
-        database="locator_api_db",
-        host="db"
+        database=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host
     )
     logger.info(f'Connected to DB')
 
--- updating_locations.py
+++ updating_locations.py
@@ -1,12 +1,22 @@
 import asyncio
 import logging.config
+import os
 
 import asyncpg
+from dotenv import load_dotenv
 
 from db.trucks import update_all_trucks_locations
 
 logging.config.fileConfig('etc/logging.conf')
 logger = logging.getLogger('Location_updater')
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
 
 
 async def update_locations_periodically():
@@ -15,10 +25,10 @@ async def update_locations_periodically():
     машин раз в 3 минуты (локация меняется на другую случайную)
     """
     conn = await asyncpg.connect(
-        database="locator_api_db",
-        user="postgres",
-        password="postgres",
-        host="db"
+        database=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host
     )
     while True:
         await asyncio.sleep(180)
